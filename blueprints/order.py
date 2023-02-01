552bd9774079a5cce1d7a812ef1e66721f10af35
LiuWH <65588374+Weihong-Liu@users.noreply.github.com>
2023-02-01 13:04:31 +0800

    添加订单号获取页面

--- blueprints/order.py
+++ blueprints/order.py
@@ -1,13 +1,18 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from snowflake import client
 
 order = Blueprint("order", __name__, url_prefix="/order")
+
+# 订单号页面
+@order.route('/', methods=['GET'])
+def to_order():
+    return render_template('order.html')
 
 # 生成订单号接口
 @order.route('/get_order', methods=['GET'])
 def get_order():
     try:
-        return jsonify(msg="success", order_id=client.get_guid())  # 雪花算法，服务器要先执行snowflake_start_server --worker=3
+        return jsonify(msg="success", order_id=str(client.get_guid()))  # 雪花算法，服务器要先执行snowflake_start_server --worker=3
     except Exception as e:
         print(e)
         return jsonify(msg="error")
