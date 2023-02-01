73fcccde2294a7806705f25470faeacfb5d4fe9e
LiuWH <65588374+Weihong-Liu@users.noreply.github.com>
2023-02-01 11:49:09 +0800

    订单接口完善捕获异常

--- blueprints/order.py
+++ blueprints/order.py
@@ -6,6 +6,8 @@ order = Blueprint("order", __name__, url_prefix="/order")
 # 生成订单号接口
 @order.route('/get_order', methods=['GET'])
 def get_order():
-    return jsonify(msg="success", order_id=client.get_guid())  # 雪花算法，服务器要先执行snowflake_start_server --worker=3
-
-
+    try:
+        return jsonify(msg="success", order_id=client.get_guid())  # 雪花算法，服务器要先执行snowflake_start_server --worker=3
+    except Exception as e:
+        print(e)
+        return jsonify(msg="error")
